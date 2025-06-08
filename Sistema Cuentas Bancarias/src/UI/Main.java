c4ee4a8ada7ddc5b759cb8d45af3bb73c0e4863a
Sebas2989 <svalverdema@ucenfotec.ac.cr>
2025-06-08 17:48:26 -0600

    Main

--- Sistema Cuentas Bancarias/src/UI/Main.java
+++ Sistema Cuentas Bancarias/src/UI/Main.java
@@ -1,7 +1,9 @@
 package UI;
 
-import BL.Carrera;
-import BL.Estudiante;
+import BL.Administrador;
+import BL.Cliente;
+import BL.CuentaBancaria;
+import BL.Transacciones;
 
 import java.time.LocalDate;
 
@@ -11,50 +13,24 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Carrera carrera = new Carrera();
-        carrera.setCodigo(5);
-        carrera.setNombre("Bisoft");
-        carrera.setCredito(4);
-        carrera.setEscuela("Cenfotec");
-        carrera.setDuración(2.5);
-        System.out.println(carrera.toString());
+        Administrador admin = new Administrador("Pedro", "Araya", "Perez", "110145643", "ejemplo@cenfotec.ac.cr");
+        Cliente cliente1 = new Cliente("CL001", "Laura", "Hernández", "Ramírez", "203040506", "Femenino", "laura.hrz@example.com", "Ingeniera Civil", "San José, Costa Rica");
+        CuentaBancaria cuenta1 = new CuentaBancaria( "CT001", "Ahorro", 150.00, "Activa");
+        Transacciones transacciones1 = new Transacciones( LocalDate.parse("2025-06-08"),200.00, 1001, "Depósito" );
 
-        Estudiante estudiante = new Estudiante("1234","Juan","Perez","Garcia","Moravia","0123456789","<EMAIL>", LocalDate.parse("2000-01-31"),true);
-        Estudiante estudiante1 = new Estudiante("12341","Juan1","Perez1","Garcia","Moravia","0123456789","<EMAIL>", LocalDate.parse("2000-01-31"),true);
-        Estudiante estudiante2 = new Estudiante("1234","Juan1","Perez","Garcia");
 
-        System.out.println(estudiante.toString());
-        System.out.println(estudiante1.toString());
-        System.out.println(estudiante2.toString());
+        System.out.println(admin);
+        System.out.println(cliente1);
+        System.out.println(cuenta1);
+        System.out.println(transacciones1);
 
-        String x = "hola";
-        String y = "hola";
-        String z = "nuevo";
-        System.out.println(estudiante.equals(estudiante1));
+
 
 
 
 
     }
 
-    public void miMetodo()
-    {
-        System.out.println("Hola mundo");
-    }
-
-    public void miMetodo(String nombre)
-    {
-        System.out.println("Hola mundo"+nombre);
-    }
-    public void miMetodo(String nombre,String apellido)
-    {
-        System.out.println("Hola mundo"+nombre+apellido);
-    }
-
-    public void miMetodo(String nombre,int apellido)
-    {
-        System.out.println("Hola mundo"+nombre+apellido);
-    }
 
     }
 
